18af664a99dff7c57691061495788cf98e27ee9f
Ivan Sanchez <ivanhumberto_16@hotmail.com>
2022-06-18 21:09:15 -0600

    Project initialize

---
+++ src/main/java/com/pickemsystem/pickemsystembackend/PickemsystembackendApplication.java
@@ -0,0 +1,13 @@
+package com.pickemsystem.pickemsystembackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickemsystembackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PickemsystembackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pickemsystem/pickemsystembackend/PickemsystembackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pickemsystem.pickemsystembackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickemsystembackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
